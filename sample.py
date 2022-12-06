a32283689ab007aafb8f89b6a354c05b63e204b3
Marco <m.limantara@travanto.de>
2022-12-06 23:57:31 +0100

    unittest test

--- sample.py
+++ sample.py
@@ -1,4 +1,15 @@
-a = 1
-b = 2
-c = a+b
-print("result is", c)
+#a = 1
+#b = 2
+
+
+#c = a+b
+#print("result is", c)
+
+x = int(input("x? "))
+y = int(input("y? "))
+
+if x > 10:
+    if y == x:
+        print("Fine")
+    else:
+        print("So what?")
